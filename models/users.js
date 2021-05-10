e0bdfab623e03e4a25c394cc898d5700d31c7749
ahmedKhaled1995 <ahmed@DESKTOP-Q38GDJO.localdomain>
2021-05-11 01:05:19 +0200

    Added user schema

---
+++ models/users.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Task = require("./task");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is invalid!");
+            }
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (validator.contains(value, "password")) {
+                throw new Error('Password can\'t contain \'password\'!');
+            }
+        }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        require: true,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+    avatar: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
+});
+
+// defining model for users table
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
